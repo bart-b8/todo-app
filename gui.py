74219ab4291b9b73ac6de6830a36653429a516ed
bart-b8 <68114296+bart-b8@users.noreply.github.com>
2022-11-27 12:27:52 +0100

    Created edit functionality and automatical refres after actions

--- gui.py
+++ gui.py
@@ -4,8 +4,10 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
 window = sg.Window('My To-Do App', 
-                   layout=[[label], [input_box, add_button]], 
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]], 
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -16,6 +18,17 @@ while True:
             todos = functions.get_todos()
             todos.append(values['todo'] + "\n")
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            todo = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo)
+            todos[index] = new_todo + '\n'
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
